336e7a8e71d4979953cab15260ee6c48decab4cc
Zhumashova Saadat <saadat9195@gmail.com>
2024-05-31 12:23:02 +0300

    Теперь админ может опубликовать и удалять коктейли.

--- frontend/src/features/Cocktails/Cocktails.tsx
+++ frontend/src/features/Cocktails/Cocktails.tsx
@@ -1,5 +1,7 @@
 import {useEffect} from 'react';
 import {
+    Button,
+    Card,
     CardContent,
     CardMedia,
     CircularProgress,
@@ -10,7 +12,8 @@ import {
 import {selectUser} from '../Users/usersSlice.ts';
 import { useAppDispatch, useAppSelector } from '../../../App/hooks';
 import { selectCocktails, selectIsLoading } from './coctailsSlice';
-import { getCocktails } from './coctailsThunks';
+import { deleteCocktail, getCocktails, publishCocktail } from './coctailsThunks';
+import { apiUrl } from '../../consttants';
 
 const Cocktails = () => {
     const dispatch = useAppDispatch();
@@ -31,38 +34,73 @@ const Cocktails = () => {
         paddingTop: '56.25%',
     });
 
+    const deleteOneCocktail = async (id: string) => {
+        await dispatch(deleteCocktail(id));
+        await dispatch(getCocktails());
+    };
+
+    const publish = async (id: string) => {
+        await dispatch(publishCocktail(id));
+        await dispatch(getCocktails());
+    };
+
     return (
         <>
             <Grid container spacing={3}>
                 {!isLoading ? cocktails.map((elem) => (
                     <Grid item xs={6} key={elem._id}>
-                        {elem.image !== null ? <ImageCardMedia image={`http://localhost:8000/${elem.image}`}/> : ''}
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div">
-                                {elem.name}
+                        {user && user.role === 'admin' ? <Card>
+                            {elem.image ? <ImageCardMedia image={`${apiUrl}/${elem.image}`}/> : ''}
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {elem.name}
+                                </Typography>
+                                <Typography gutterBottom component="div">
+                                    {elem.ingredients.map((ingredient) => (
+                                        <Grid key={ingredient._id}>
+                                            <Typography component="div">
+                                                {`name: ${ingredient.ingredientName}, quantity: ${ingredient.quantity}`}
+                                            </Typography>
+                                        </Grid>
+                                    ))}
+                                </Typography>
+                                <Typography gutterBottom component="div">
+                                    Recipe: {elem.recipe}
+                                </Typography>
+                                <Typography gutterBottom component="div">
+                                    Статус: {!elem.isPublished ? 'не опубликовано' : 'опубликовано'}
+                                </Typography>
+                            </CardContent>
+                            <Typography component="div" sx={{display: 'flex', justifyContent: 'space-between'}}>
+                                <Button sx={{color: 'red'}} onClick={() => deleteOneCocktail(elem._id)}>Удалить</Button>
+                                <Button
+                                    onClick={() => publish(elem._id)}>{elem.isPublished ? 'Снять с публикации' : 'Опубликовать'}</Button>
                             </Typography>
-                            <Typography gutterBottom component="div">
-                                {elem.ingredients.map((ingredient) => (
-                                    <Grid key={ingredient._id}>
-                                        <Typography component="div">
-                                            {`name: ${ingredient.ingredientName}, quantity: ${ingredient.quantity}`}
-                                        </Typography>
-                                    </Grid>
-                                ))}
-                            </Typography>
-                            <Typography gutterBottom component="div">
-                                Recipe: {elem.recipe}
-                            </Typography>
-
-                            <Typography gutterBottom component="div">
-                                Статус: {!elem.isPublished ? 'ваш коктейль находится на рассмотрении модератора' : 'опубликовано'}
-                            </Typography>
-                        </CardContent>
-
+                        </Card> : elem.isPublished ? <Card>
+                            {elem.image ? <ImageCardMedia image={`${apiUrl}/${elem.image}`}/> : ''}
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {elem.name}
+                                </Typography>
+                                <Typography gutterBottom component="div">
+                                    {elem.ingredients.map((ingredient) => (
+                                        <Grid key={ingredient._id}>
+                                            <Typography component="div">
+                                                {`name: ${ingredient.ingredientName}, quantity: ${ingredient.quantity}`}
+                                            </Typography>
+                                        </Grid>
+                                    ))}
+                                </Typography>
+                                <Typography gutterBottom component="div">
+                                    Recipe: {elem.recipe}
+                                </Typography>
+                            </CardContent>
+                        </Card> : ''}
                     </Grid>
-                )) : <CircularProgress />}</Grid>
+                )) : <CircularProgress/>}
+            </Grid>
         </>
     );
-};
+}
 
 export default Cocktails;
--- frontend/src/features/Cocktails/coctailsThunks.ts
+++ frontend/src/features/Cocktails/coctailsThunks.ts
@@ -12,7 +12,28 @@ export const getCocktails = createAsyncThunk<CocktailTypes[]>(
         if (!items) {
             return [];
         }
-
         return items;
+    },
+);
+export const publishCocktail = createAsyncThunk<void, string>(
+    'publish/cocktail',
+    async (id) => {
+        try {
+            await axiosApi.patch('/cocktails/' + id + '/togglePublished');
+        } catch (err) {
+            throw err;
+        }
+        },
+);
+
+
+export const deleteCocktail = createAsyncThunk<void, string>(
+    'delete/cocktail',
+    async (id) => {
+        try {
+            await axiosApi.delete('/cocktails/' + id);
+        } catch (err) {
+            throw err;
+        }
     },
 );
